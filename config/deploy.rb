b5547546c18e709943857c9909e043537d051e60
taka0707 <yukken777@gmail.com>
2020-07-01 14:57:37 +0900

    edit-deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,9 +1,9 @@
 # config valid only for current version of Capistrano
 # capistranoのバージョンを記載。固定のバージョンを利用し続け、バージョン変更によるトラブルを防止する
-lock '<3.14.1>'
+lock '3.14.1'
 
 # Capistranoのログの表示に利用する
-set :application, '<chat-space>'
+set :application, 'chat-space'
 
 # どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:taka0707/chat-space.git'
@@ -12,7 +12,7 @@ set :repo_url,  'git@github.com:taka0707/chat-space.git'
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
-set :rbenv_ruby, '<2.6.5>' #カリキュラム通りに進めた場合、2.6.5です
+set :rbenv_ruby, '2.6.5' #カリキュラム通りに進めた場合、2.6.5です
 
 # どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
